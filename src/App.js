6f9237caec7f8121c4671ec2bffea1f516893964
Timi <itstimiking@gmail.com>
2021-12-01 11:43:30 +0100

    price fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
 
     const [tourData, setTourData] = useState([])
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
 
     useEffect(() => {
         const getTours = async() => {
@@ -35,7 +34,7 @@ function App() {
     }
 
     return tourData && (
-        <main className="flex items-center w-full min-h-screen flex-col my-10 md:my-24 text-gray-700">
+        <main className="flex items-center w-full min-h-screen flex-col my-5 md:my-24 text-gray-700">
 
             <h1 className="font-black text-4xl border-b-4 border-blue-400 mb-10"> 
                 Our Tours 
